639a93b39b230bf31edb995785956af414b0d0cd
KN-Kevinoh <lekankevinoh@gmail.com>
2020-12-26 17:35:03 +0700

    updapte project

---
+++ Casse_briques/CasseBrique.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Casse_briques
+{
+    public class CasseBrique : Game
+    {
+        private readonly GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        // Déclaration des variables
+        private Texture2D background;
+        private Raquette raquette;
+        private Balle balle;
+
+        public CasseBrique()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = false;
+        }
+
+        protected override void Initialize()
+        {
+            this._graphics.IsFullScreen = false;
+            this._graphics.PreferredBackBufferWidth = 650;
+            this._graphics.PreferredBackBufferHeight = 600;
+            this._graphics.ApplyChanges();
+
+            balle = new Balle(this, "balle", raquette);
+            this.Components.Add(balle);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            background = this.Content.Load<Texture2D>("background");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            this._spriteBatch.Begin();
+
+            this._spriteBatch.Draw(background, new Vector2(0, 0), Color.Gray);
+
+            this._spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
