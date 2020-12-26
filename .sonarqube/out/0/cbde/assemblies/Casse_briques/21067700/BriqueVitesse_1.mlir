545bf5e61a73582e77b0b703e8ba83b059360318
Franck Anael <franelstar@gmail.com>
2020-12-26 20:29:06 +0700

    Gestion des briques

---
+++ .sonarqube/out/0/cbde/assemblies/Casse_briques/21067700/BriqueVitesse_1.mlir
@@ -0,0 +1,20 @@
+func @_Casse_briques.BriqueVitesse.Initialize$$() -> () loc("C:\\Users\\Franel\\source\\repos\\Casse_briques\\Casse_briques\\BriqueVitesse.cs" :13 :8) {
+^entry :
+br ^0
+
+^0: // SimpleBlock
+%0 = cbde.unknown : none loc("C:\\Users\\Franel\\source\\repos\\Casse_briques\\Casse_briques\\BriqueVitesse.cs" :15 :12) // base (BaseExpression)
+%1 = cbde.unknown : none loc("C:\\Users\\Franel\\source\\repos\\Casse_briques\\Casse_briques\\BriqueVitesse.cs" :15 :12) // base.Initialize() (InvocationExpression)
+%2 = constant 30 : i32 loc("C:\\Users\\Franel\\source\\repos\\Casse_briques\\Casse_briques\\BriqueVitesse.cs" :16 :20)
+%3 = constant unit loc("C:\\Users\\Franel\\source\\repos\\Casse_briques\\Casse_briques\\BriqueVitesse.cs" :17 :37) // 0.02f (NumericLiteralExpression)
+%4 = cbde.unknown : none loc("C:\\Users\\Franel\\source\\repos\\Casse_briques\\Casse_briques\\BriqueVitesse.cs" :18 :30) // this (ThisExpression)
+%5 = cbde.unknown : none loc("C:\\Users\\Franel\\source\\repos\\Casse_briques\\Casse_briques\\BriqueVitesse.cs" :18 :30) // this.Game (SimpleMemberAccessExpression)
+%6 = cbde.unknown : none loc("C:\\Users\\Franel\\source\\repos\\Casse_briques\\Casse_briques\\BriqueVitesse.cs" :18 :30) // this.Game.Content (SimpleMemberAccessExpression)
+%7 = cbde.unknown : none loc("C:\\Users\\Franel\\source\\repos\\Casse_briques\\Casse_briques\\BriqueVitesse.cs" :18 :66) // "sons/collision_brique_vitesse" (StringLiteralExpression)
+%8 = cbde.unknown : none loc("C:\\Users\\Franel\\source\\repos\\Casse_briques\\Casse_briques\\BriqueVitesse.cs" :18 :30) // this.Game.Content.Load<SoundEffect>("sons/collision_brique_vitesse") (InvocationExpression)
+br ^1
+
+^1: // ExitBlock
+return
+
+}
---
+++ Casse_briques/MurDeBrique.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Casse_briques
+{
+    class MurDeBrique : GameComponent
+    {
+        private List<Brique> briques;
+        private readonly int width;
+        private readonly int height;
+        private int level;
+
+        public MurDeBrique(Game game) : base(game)
+        {
+            width = 8;
+            height = 15;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            level = 0;
+            this.briques = new List<Brique>();
+        }
+
+        public int startLevel(int maxLevel, Balle balle, Raquette raquette)
+        {
+            // On verifie si toutes les briques sont désactives
+            List<Brique> briques_old = new List<Brique>();
+
+            foreach (var brique in briques)
+            {
+                if (brique.active)
+                    briques_old.Add(brique);
+            }
+            briques.Clear();
+            briques = briques_old;
+
+            // Si le nombre de brique est 0
+            if (briques.Count == 0)
+            {
+                level++;
+                raquette.reset();
+
+                if (level > maxLevel)
+                    return level;
+
+                int[,] level_position = getLevel("levels/" + level + ".txt");
+
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        if (level_position[x, y] == 1)
+                        {
+                            Brique brique = new Brique(base.Game, "brique", new Vector2(x * 70 + 9 * x + 13, y * 15 + 8 * y + 8));
+                            briques.Add(brique);
+                            this.Game.Components.Add(brique);
+                        }
+                        else if (level_position[x, y] == 2)
+                        {
+                            BriqueVitesse brique = new BriqueVitesse(base.Game, "briqueVitesse", new Vector2(x * 70 + 9 * x + 13, y * 15 + 8 * y + 8));
+                            briques.Add(brique);
+                            this.Game.Components.Add(brique);
+                        }
+                    }
+                }
+            }
+
+            return level;
+        }
+
+        int[,] getLevel(string fileName)
+        {
+            using (StreamReader reader = new StreamReader(File.OpenRead(fileName)))
+            {
+                int[,] result = new int[width, height];
+
+                for (int x = 0; x < height; x++)
+                {
+                    string line = reader.ReadLine();
+                    string[] col = line.Split(' ');
+                    for (int y = 0; y < width; y++)
+                    {
+                        result[y, x] = int.Parse(col[y]);
+                    }
+                }
+
+                return result;
+            }
+        }
+    }
+}
