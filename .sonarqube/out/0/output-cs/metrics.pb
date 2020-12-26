e9c50f60ea7284d1594cb12bb325b247f686a272
Franck Anael <franelstar@gmail.com>
2020-12-26 23:57:15 +0700

    Modification des niveaux et ajouts des fihier de rapport

---
+++ .sonarqube/out/0/output-cs/metrics.pb
@@ -0,0 +1,22 @@
+Î
+AC:\Users\Franel\source\repos\Casse_briques\Casse_briques\Balle.cs/ 8j)7>GXgrb	
+ !"#$%'(+-./0123489:;<?@ABCDHIJKLNOPQSTVWYZ[\]^_`bcefhijklmnoprsuvwxyz{|}xÇ/ !"#$+-/01248:;?@BCHJKNPSY[\]^_bhklmnorwxyzÄ
+BC:\Users\Franel\source\repos\Casse_briques\Casse_briques\Brique.cs 8r(	
+ "#$%&'(*+,-./Ç&,o
+IC:\Users\Franel\source\repos\Casse_briques\Casse_briques\BriqueVitesse.cs 8r	
+Çﬂ
+GC:\Users\Franel\source\repos\Casse_briques\Casse_briques\CasseBrique.csK 8jPUhxrü	
+ !#$%&'(*,-/02356789:;=>@ACDEGHIJLMNOQRSVWXYZ[\]_`acdefijklmnopqrsuvy{|}~ÄÅÇÉÑÖÜáàâåçèêëìîñòôõúùû†¢£§•xÇ` %&'(*,-/02356789:;=>@EGHINQSVXYZ[\]_cefijmopqrsuy{}~ÄÅÇÉÑÖÜáâåëìîñòôõù†¢ë
+EC:\Users\Franel\source\repos\Casse_briques\Casse_briques\Element2D.cs 8r.	
+ !"#$%&()+,-./01345xÇ!#$%(/Ü
+IC:\Users\Franel\source\repos\Casse_briques\Casse_briques\ElementText2D.cs 8r#	
+ !"#$&'()Ç"#$&f
+?C:\Users\Franel\source\repos\Casse_briques\Casse_briques\Hud.cs 8r
+ÇÙ
+GC:\Users\Franel\source\repos\Casse_briques\Casse_briques\MurDeBrique.cs1 8j +rj	
+!#$%&'(),-./0235789:;<=>?@ABCDEFGHIJKMNPQRSTVWXYZ[\]^`abdefhijklmnopqstuvwxyz{|xÇ.#%&(),./023579;=>?ACDEIJMRVXYZ\`hjlmpuwya
+CC:\Users\Franel\source\repos\Casse_briques\Casse_briques\Program.cs 8r	
+Ç
+å
+DC:\Users\Franel\source\repos\Casse_briques\Casse_briques\Raquette.cs 8	r+	
+ !"#$%&')*,-./01xÇ "$).
---
+++ Casse_briques/Balle.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Casse_briques
+{
+    class Balle : Element2D
+    {
+        private Vector2 direction;
+        private float speed;
+        private readonly Raquette raquette;
+        private KeyboardState _keyboardState;
+        private GameStates CurrentGameState;
+        protected SoundEffect _ballBounceMur;
+        protected SoundEffect _ballBounceLife;
+
+        public enum GameStates { Paused, Playing }
+
+        public Balle(Game game, String texture, Raquette raquette) : base(game, texture, new Vector2(0, 0))
+        {
+            this.raquette = raquette;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            
+            position = new Vector2(_screenWidth / 2 - texture2D.Width / 2, _screenHeight - texture2D.Height / 2 - 48);
+
+            CurrentGameState = GameStates.Paused;
+            _keyboardState = Keyboard.GetState();
+            direction = new Vector2(1, -1);
+            speed = 0.1f;
+            _ballBounceMur = this.Game.Content.Load<SoundEffect>("sons/collision_mur");
+            _ballBounceLife = this.Game.Content.Load<SoundEffect>("sons/life");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // On vérifie si l'utilisateur change l'état du jeu
+
+            KeyboardState currentState = Keyboard.GetState();
+
+            if (_keyboardState.IsKeyDown(Keys.Space) && currentState.IsKeyUp(Keys.Space))
+            {
+                if (CurrentGameState == GameStates.Playing)
+                    CurrentGameState = GameStates.Paused;
+                else if (CurrentGameState == GameStates.Paused)
+                    CurrentGameState = GameStates.Playing;
+            }
+            _keyboardState = currentState;
+
+
+            // On fait rebondir la balle si elle touche le haut de l'écran
+            if (position.Y <= 0 && direction.Y < 0)
+            {
+                direction = new Vector2(direction.X, -direction.Y);
+                _ballBounceMur.Play();
+            }
+
+            // On fait rebondir la balle si elle touche la gauche ou la droite de l'écran
+            if ((position.X <= 0 && direction.X < 0)
+                || (position.X > _screenWidth - texture2D.Width && direction.X > 0))
+            {
+                direction = new Vector2(-direction.X, direction.Y);
+                _ballBounceMur.Play();
+            }
+
+
+            // Si la balle touche la raquette, on la fait changer de sens
+            if (direction.Y > 0 && raquette.rectangle.Intersects(new Rectangle((int)position.X, (int)position.Y, texture2D.Width, texture2D.Height)))
+            {
+                direction = new Vector2(direction.X, -direction.Y);
+                _ballBounceMur.Play();
+            }
+
+            if(CurrentGameState == GameStates.Playing)
+            {
+                position += direction * speed * (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+            }
+
+            base.Update(gameTime);
+        }
+
+        public bool collision_brique(Brique brique, float multiplicateur_vitesse)
+        {
+            // Si la balle touche la brique, on la fait changer de sens
+            if (brique.rectangle.Intersects(new Rectangle((int)position.X, (int)position.Y, texture2D.Width, texture2D.Height)))
+            {
+                brique.playSon();
+                direction = new Vector2(direction.X, -direction.Y);
+                speed += multiplicateur_vitesse;
+                brique.active = false;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool perte()
+        {
+            // Si la balle touche le fond de l'écran
+            if (position.Y > _screenHeight - texture2D.Height && direction.Y > 0)
+            {
+                float old_speed = speed;
+                _ballBounceLife.Play();
+                reset();
+                speed = old_speed;
+                raquette.reset();
+                return true;
+            }
+
+            return false;
+        }
+
+        public void reset()
+        {
+            CurrentGameState = GameStates.Paused;
+            position = new Vector2(_screenWidth / 2 - texture2D.Width / 2, _screenHeight - texture2D.Height / 2 - 48);
+            direction = new Vector2(1, -1);
+            speed = 0.1f;
+        }
+    }
+}
